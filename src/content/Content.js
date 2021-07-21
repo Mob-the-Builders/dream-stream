8c71196a003ba38b04838b4b0aabc0c52a380eed
“sashinshin” <sasha.luhr@gmail.com>
2021-07-21 15:21:05 +0200

    working on css

--- src/content/Content.js
+++ src/content/Content.js
@@ -1,100 +1,13 @@
 import React, { useState, useEffect } from "react"
 import YourStreams from './YourStreams';
 import PostList from './Postlist';
-import axios from 'axios';
-// Remove filename after upload
-// How to break multiple tags (Tag parser)
-// upload fetchimage/cloudinary to heroku ----> future
-// 
+
 const Content = () => {
-  const [name, setName] = useState('Kalle');
-  const [tags, setTags] = useState([]);
-  const [description, setDescription] = useState('');
-  const [uploadImage, setUploadImage] = useState('');
-  const [image, setImage] = useState({
-    url: null,
-    signature: null,
-  });
-  
-  const fetchImage = async () => {
-    const formData = new FormData();
-    formData.append('file', uploadImage);
-    formData.append('upload_preset', "qxmdpdn1");
-    const data = await axios.post("https://api.cloudinary.com/v1_1/dx50vyks7/image/upload", formData)
-    setImage({
-      url: data.data.secure_url,
-      signature: data.data.signature
-    })
-  }
-
-  useEffect(() => {
-    if (image.url && image.signature) {
-      serverCall()
-    };
-  }, [image]);
-
-  const resetValues = () => {
-    setDescription('');
-    setTags('');
-    setImage({
-      url: null,
-      signature: null,
-    })
-  }
-
-  const serverCall = async () => {
-  const response = await axios.post("/api/create-post", {
-      "userName": name,
-      "image": image.url,
-      "imageDelete": image.signature,
-      "description": description,
-      "tags": [tags],
-    },
-    { 
-      headers: { 
-      "Content-Type": "application/json"
-      } 
-    });
-    
-    resetValues();
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (!tags) {
-      alert('PleaseTag');
-      return;
-    }
-   fetchImage(); 
-  };
 
   return (
-      <>
-      <form onSubmit={onSubmit}>
-
-        <label>Name</label>
-        <input type="text" id="Name"
-          placeholder='Kalle'
-          value={name}
-          onChange={e => setName(e.target.value)} />
-
-        <label>Description</label>
-        <input  type="text" id="Name"
-          placeholder='Description'
-          value={description}
-          onChange={e => setDescription(e.target.value)} />
-
-        <label>Tags</label>
-        <input required  type="text" id="Name"
-          placeholder='tag'
-          value={tags}
-          onChange={e => setTags(e.target.value)} />
-        <input required type='file' onChange={e=>setUploadImage(e.target.files[0])} />
-        <input type='submit' value='Create'/>
-      </form>
-      
-      <PostList />
-      </>
+      <main>
+        <PostList />
+      </main>
   )
 }
 
