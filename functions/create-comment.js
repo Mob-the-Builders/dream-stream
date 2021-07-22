895674db3db261e501f2e2a52a906e9774b9f1a4
kwdi <kostas.droid@gmail.com>
2021-07-22 08:59:57 +0200

    create comment

---
+++ functions/create-comment.js
@@ -0,0 +1,37 @@
+// create-comment.js
+
+const query = require("./utils/query");
+
+const CREATE_COMMENT = `
+  mutation( $userName: String!, $message: String!, $postId: ID!){
+    createComment(data: {userName: $userName, message: $message, post: {connect: $postId} }){
+      userName
+      message
+      post{
+        userName
+        image
+        description
+      }
+    }
+  }
+`;
+
+exports.handler = async event => {
+  const { userName, message, postId } = JSON.parse(event.body);
+  const { data, errors } = await query(
+          CREATE_COMMENT, { 
+  userName, message, postId });
+
+  if (errors) {
+    console.log(errors)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors)
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ Comment: data.createComment })
+  };
+};
