ff760a64ee2743b4ae0c3a0d6c32d7b9571f47c6
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2021-07-23 22:00:04 +0200

    Sidebar CSS

---
+++ src/pages/profile.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Menubar from '../components/Menubar';
+
+
+const Profile = () => {
+  
+
+  return (
+    <div className='top-container'>
+      <Menubar />
+      <main className={'main'}>
+
+        <p>this is the profile page</p>
+
+      </main>
+    </div>
+  )
+}
+
+export default Profile;
---
+++ src/pages/streams.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Menubar from '../components/Menubar';
+
+
+const Streams = () => {
+  
+
+  return (
+    <div className='top-container'>
+      <Menubar />
+      <main className={'main'}>
+
+        <p>this is the streams page</p>
+
+      </main>
+    </div>
+  )
+}
+
+export default Streams;
