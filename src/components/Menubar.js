a43e8cff6d812f4bf0e77812c941cd8f976a3ec1
Max Anderberg <maxanderberg@gmail.com>
2021-07-24 10:56:13 +0200

    Added ssr window coverage for menubar

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -6,6 +6,8 @@ import Logout from '../content/Logout';
 import Login from '../content/Login';
 
 const Menubar = ({page}) => {
+    const user = typeof window !== 'undefined' ? localStorage.getItem('user') : null;
+
     return (
         <div className="sidebar">
           <Logo />
@@ -17,7 +19,7 @@ const Menubar = ({page}) => {
             <li><Link to="/new-post" className={`menuList__link  ${page == 'newpost' ? 'menuList__link--selected' : ''}`}><svg className={`menuList__svg  ${page == 'newpost' ? 'menuList__svg--selected' : ''}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M20 2H8c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zM8 16V4h12l.002 12H8z"/><path d="M4 8H2v12c0 1.103.897 2 2 2h12v-2H4V8zm11-2h-2v3h-3v2h3v3h2v-3h3V9h-3z"/></svg>New Post</Link> </li>
           </ul>
 
-          {localStorage.getItem('user')
+          {user
             ? <div className={'logout'}><Logout /></div>
             : <div className={'login'}><Login /></div>}
         </div>
