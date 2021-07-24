5b850261b4512adbb5333984216a3d5fc5dc54b5
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2021-07-24 12:53:08 +0200

    Post CSS

--- src/pages/streams.js
+++ src/pages/streams.js
@@ -3,11 +3,13 @@ import Menubar from '../components/Menubar';
 import { navigate } from '@reach/router';
 
 const Streams = () => {
+
   const user = typeof window !== 'undefined' ? localStorage.getItem('user') : null;
   if(!user){
     navigate('/login');
   }
   // if (!localStorage.getItem('user')) navigate('/login');
+
   return (
     <div className='top-container'>
       <Menubar page={'streams'}/>
