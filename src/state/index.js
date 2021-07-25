106cfc6dfcf58ffe2def98b3d7f2071973b6bc5c
Max Anderberg <maxanderberg@gmail.com>
2021-07-25 15:28:28 +0200

    fixed import

--- src/state/index.js
+++ src/state/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
 import postsReducer from './postsSlice';
-import userReducer from './userSlice';
+import userReducer from './UserSlice';
 
 export default combineReducers({ postList: postsReducer, user: userReducer });
