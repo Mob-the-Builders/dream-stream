b8ce6cfdc3c06bc16a2f3d71ca9e36e191eb4dd1
Max Anderberg <maxanderberg@gmail.com>
2021-07-26 22:47:33 +0200

    added loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import './Loader.scss';
+const Loader = () => {
+  return (
+    <div>
+      <div className="overlay">
+        <div className="loader-container">
+          <div className="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
